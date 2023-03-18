68ff12bc838ee969b75dedeb4345b2a83359b539
Dr.NooB <itsdrnoob@gmail.com>
2023-03-19 01:01:11 +0530

    Updated network diagnostics
    fix: IllegalStateException
    
    fix: ClassNotFoundException on java.time in API 25 and below
    
    feat: switched to DataMonitorWeb [https://github.com/itsdrnoob/DataMonitorWeb]
    
    Other improvements and bug fixes

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -6,7 +6,13 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_drnoob_datamonitor_ui_fragments_NetworkDiagnosticsFragment_getApiKey(JNIEnv *env,
-                                                                              jobject instance) {
+jobject instance) {
     // TODO: implement getApiKey()
     return (*env)->  NewStringUTF(env, "NDIwYTI1Yzk4ZDk3MTc="); // F-Droid // Replace with Base64 encoded Api key from https://ipinfo.io/
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_drnoob_datamonitor_ui_fragments_NetworkDiagnosticsFragment_getToken(JNIEnv *env,
+jobject instance) {
+    return (*env)->  NewStringUTF(env, "YjU4NTg2NDctYWEzMS00NTkxLWFjYWQtNWYxMzNiM2QyYmI2");
 }
